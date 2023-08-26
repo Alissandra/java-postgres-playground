8d76b1b6068f4ecd9a17cbbf100ffd00cda72ad8
Alissandra Cabral <alissandracabral.dev@gmail.com>
2023-08-26 04:37:06 +0000

    Testes JUnit 5

--- src/main/java/com/example/MathUtil.java
+++ src/main/java/com/example/MathUtil.java
@@ -7,12 +7,23 @@ public class MathUtil {
      * não será preciso instaciar a classe, basta chamar a apartir
      * classe diretamente.
      */
-
+                            //11  e  2
     public static int mdc(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
         if(b > 0 && a % b == 0) {
             return b;
         }
-        return 0;
+        if(b == 0) {
+            return Math.abs(a);
+        }
+        if(a % b > 0 && b % a == 0) {
+            return a;
+        }
+        return -1; //usar -1 p ver retornou erro pq se usar 0 pode ser um valor válido para algum cenário
+        //throws 
     }
+
+    
 
 }
--- src/test/java/com/example/MathUtilTest.java
+++ src/test/java/com/example/MathUtilTest.java
@@ -6,11 +6,92 @@ import org.junit.jupiter.api.Test;
 
 public class MathUtilTest {
     @Test
-    void testMdc() {
+    void testMdcP1BImpar() {
         int a = 6, b = 3;
         int esperado = 3;
         int obtido = MathUtil.mdc(a, b);
 
         assertEquals(esperado, obtido);
     }
+
+    @Test
+    void testMdcP1BPar() {
+        int a = 6, b = 2;
+        int esperado = 2;
+        int obtido = MathUtil.mdc(a, b);
+
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testMdcP3Negativo() {
+        int a = -6, b = 0;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, b);
+
+        assertEquals(esperado, obtido);
+    }
+    @Test
+    void testMdcP3Positivo() {
+        int a = 6, b = 0;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, b);
+
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testMdcP5() {
+        int a = 2, b = 6;
+        int esperado = MathUtil.mdc(b, a);
+        int obtido = MathUtil.mdc(a, b);
+
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testMdcP7() {
+        int a = 2, b = 6;
+        int esperado = MathUtil.mdc(-b, a);
+        int obtido = MathUtil.mdc(-a, -b);
+
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testMdcP8() {
+        int a = 6;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, a);
+
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testMdcP12A() {
+        int p = 11, a = 22;
+        int esperado = 11;
+        int obtido = MathUtil.mdc(p, a);
+
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testMdcP12B() {
+        int p = 11, a = 1;
+        int esperado = 1;
+        int obtido = MathUtil.mdc(p, a);
+
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testMdcP12C() {
+        int p = 11, a = 2;
+        int esperado = 1;
+        int obtido = MathUtil.mdc(p, a);
+
+        assertEquals(esperado, obtido);
+    }
+    
 }
